cad6efdcaa4892290dc0d0e3f872961d1a8d918c
eurekasolution <kjmin@daum.net>
2024-09-05 12:34:12 +0900

    D1235

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.js"></script>
     <style>
 
     </style>

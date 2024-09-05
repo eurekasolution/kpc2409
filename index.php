2dc20aed08ccd6b35b3a815e7f95dd43e6263bcb
eurekasolution <kjmin@daum.net>
2024-09-05 09:44:34 +0900

    D0945

--- index.php
+++ index.php
@@ -17,6 +17,9 @@
     <title>취약점 분석</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    
     <style>
         /* Flexbox 레이아웃 설정 */
         html, body {
